58aba14abd29e00f37c05988482fb241a919b741
saph <saph102@gmail.com>
2016-05-08 00:45:20 +0300

    Added files via upload

--- Code/BuildDB/createMySQLTableFromCSV.py
+++ Code/BuildDB/createMySQLTableFromCSV.py
@@ -29,84 +29,87 @@ def checkType(data):
 # In[70]:
 
 # write the sql file from data file
-def writeDataSqlFile(f,data,tableName,valueTypeArray):
+def writeDataSqlFile(fscheme,fdata,data,tableName,valueTypeArray):
     
-    f.write("CREATE TABLE %s (\n" % tableName)
+    fscheme.write("CREATE TABLE %s (\n" % tableName)
     headline=data.pop(0)
         
     # create table
-    for i in range (0,len(headline)):
+    fscheme.write("\tID INT NOT NULL AUTO_INCREMENT,\n")
+    for i in range (0,len(headline)-1):
         item=headline[i]
         item=item.replace(" ","_")
         if valueTypeArray[i]==0:
-            f.write("\t%s INT,\n" % item)
+            fscheme.write("\t%s INT,\n" % item)
         else:
             if "comment" in item or "discription" in item or valueTypeArray[i]>1500:
-                f.write("\t%s TEXT,\n" % item)
+                fscheme.write("\t%s TEXT,\n" % item)
             elif item=="name":
                 valueTypeArray[i]+=16
-                f.write("\t{0} VARCHAR({1}) NOT NULL,\n".format(item,valueTypeArray[i]))
+                fscheme.write("\t{0} VARCHAR({1}) NOT NULL,\n".format(item,valueTypeArray[i]))
             else:
                 valueTypeArray[i]+=16
-                f.write("\t{0} VARCHAR({1}),\n".format(item, valueTypeArray[i]))
-    f.write("\tPRIMARY KEY (ID)\n")
-    f.write(");\n")
+                fscheme.write("\t{0} VARCHAR({1}),\n".format(item, valueTypeArray[i]))
+    fscheme.write("\tPRIMARY KEY (ID)\n")
+    fscheme.write(");\n\n")
         
     # insert values to table
     for row in data:
-        f.write("INSERT INTO %s VALUES (" % tableName)
-        for i in range (0,len(row)):
+        fdata.write("INSERT INTO %s VALUES (NULL," % tableName)
+        for i in range (0,len(row)-1):
             item=row[i]
             item=item.replace("\'","\''")
             if valueTypeArray[i]==0:
                 item=int(item)
-                f.write("%d" % item)
+                fdata.write("%d" % item)
             else:
-                f.write('\'')
-                f.write("%s" % item)
-                f.write('\'')
-            if i<len(headline)-1:
-                f.write(",")
-        f.write(");\n")
+                fdata.write('\'')
+                fdata.write("%s" % item)
+                fdata.write('\'')
+            if i<len(headline)-2:
+                fdata.write(",")
+        fdata.write(");\n")
+    fdata.write("\n")
 
 
 # In[ ]:
 
 # write the sql file from match file
-def writeMatchSqlFile(f,data,tableName,valueTypeArray):
+def writeMatchSqlFile(fscheme,fdata,data,tableName,valueTypeArray):
    
-    f.write("CREATE TABLE %s (\n" % tableName)
+    fscheme.write("CREATE TABLE %s (\n" % tableName)
     headline=data.pop(0)
         
     # create table
     for i in range (0,len(headline)):
         item=headline[i]
         item=item.replace(" ","_")
-        f.write("\t%s INT\n" % item)
+        fscheme.write("\t%s INT\n" % item)
         refTable=item.split("_ID")[0]
-        f.write("\t\tREFERENCES %s(ID)" % refTable)
+        fscheme.write("\t\tREFERENCES %s(ID)" % refTable)
         if i<len(headline)-1:
-            f.write (',\n')
+            fscheme.write (',\n')
         else:
-            f.write ('\n')
-    f.write(");\n")
+            fscheme.write ('\n')
+    fscheme.write(");\n\n")
         
     # insert values to table
     for row in data:
-        f.write("INSERT INTO %s VALUES (" % tableName)
+        fdata.write("INSERT INTO %s VALUES (" % tableName)
         for i in range (0,len(row)):
             item=row[i]
             item=int(item)
-            f.write("%d" % item)
+            fdata.write("%d" % item)
             if i<len(headline)-1:
-                f.write(",")
-        f.write(");\n")
+                fdata.write(",")
+        fdata.write(");\n")
+    fdata.write("\n")
 
 
 # In[71]:
 
 # create tables from dir
-def createDataTable(f,dirpath):
+def createDataTable(fscheme,fdata,dirpath):
     for filename in os.listdir(dirpath):
         filepath=dirpath+'/'+filename
         tableName=filename.split('.csv')[0]
@@ -115,13 +118,13 @@ def createDataTable(f,dirpath):
             data.reverse
             f2.close
         valueTypeArray=checkType(data)
-        writeDataSqlFile(f,data,tableName,valueTypeArray)                  
+        writeDataSqlFile(fscheme,fdata,data,tableName,valueTypeArray)                  
 
 
 # In[ ]:
 
 # create tables from dir
-def createMatchTable(f,dirpath):
+def createMatchTable(fscheme,fdata,dirpath):
     for filename in os.listdir(dirpath):
         filepath=dirpath+'/'+filename
         tableName=filename.split('.csv')[0]
@@ -130,7 +133,7 @@ def createMatchTable(f,dirpath):
             data.reverse
             f2.close
         valueTypeArray=checkType(data)
-        writeMatchSqlFile(f,data,tableName,valueTypeArray)    
+        writeMatchSqlFile(fscheme,fdata,data,tableName,valueTypeArray) 
 
 
 # In[ ]:
@@ -146,19 +149,13 @@ def createIndex(f,dirpath):
 
 # write all DB building queries into one SQL_DB file
 def createSQLTables(dir1, dir2,dir3):
-    outputPath="SQL_DB/AllTablesDB.sql"
-    with open(outputPath,'w') as f:
-    #    f.write("DROP SCHEMA IF EXISTS music\n");
-    #    f.write("CREATE SCHEMA music\n");
-    #    f.write("USE music\n");
-    #   f.write("\n")
-    #    f.write("set global innodb_file_format = Barracuda;\n")
-    #    f.write("set global innodb_file_per_table = 1;\n")
-    #    f.write("\n")
-        createDataTable(f,dir1)
-        f.write("\n")
-        createMatchTable(f,dir2)
-        f.write("\n")
-        createIndex(f,dir3)
-        f.close
+    outputSchemePath="SQL_DB/musicDB_scheme.sql"
+    outputDataPath="SQL_DB/musicDB_data.sql"
+    with open(outputSchemePath,'w') as fscheme:
+        with open(outputDataPath,'w') as fdata:
+            createDataTable(fscheme,fdata,dir1)
+            createMatchTable(fscheme,fdata,dir2)
+            createIndex(fscheme,dir3)
+            fdata.close
+        fscheme.close
 
