2cd8ce3c19fd365dc76f8e91dc602d6d3027b06c
saph <saph102@gmail.com>
2016-06-02 13:23:54 +0300

    Add files via upload

--- Code/YouTube/showVideo.php
+++ Code/YouTube/showVideo.php
@@ -6,20 +6,24 @@
 <?php
 
 function getIdFromName($name){
-    $name=str_replace(" ","+",$name);
-    $url="https://www.youtube.com/results?search_query=".$name;
-    $sFile = file_get_contents($url);
-    if (strpos($sFile, 'div class="yt-lockup-content"') !== false) {
-        $myArray = explode('div class="yt-lockup-content"', $sFile,2);
-        $sFile=$myArray[1];
-        $myArray = explode('watch?v=', $sFile,2);
-        $sFile=$myArray[1];
-        $myArray = explode('"', $sFile,2);
-        $sFile=$myArray[0];
-        return ($sFile);
+    try {
+        $name = str_replace(" ", "+", $name);
+        $url = "https://www.youtube.com/results?search_query=" . $name;
+        $sFile = file_get_contents($url);
+        if (strpos($sFile, 'div class="yt-lockup-content"') !== false) {
+            $myArray = explode('div class="yt-lockup-content"', $sFile, 2);
+            $sFile = $myArray[1];
+            $myArray = explode('watch?v=', $sFile, 2);
+            $sFile = $myArray[1];
+            $myArray = explode('"', $sFile, 2);
+            $sFile = $myArray[0];
+            return ($sFile);
+        } else {
+            return ('null');
+        }
     }
-    else{
-        return (null);
+    catch (exception $e){
+        return ('null');
     }
 }
 
@@ -31,20 +35,26 @@ $sFile=getIdFromName("queen band"); //Get the id to first video for the name req
 
     // Load the IFrame Player API code asynchronously.
     urlid= "<?php echo $sFile; ?>"; // Get id from the php code.
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/player_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    if ('null' != urlid) {
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/player_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    // Replace the 'ytplayer' element with an <iframe> and
-    // YouTube player after the API code downloads.
-    var player;
-    function onYouTubePlayerAPIReady() {
-        player = new YT.Player('ytplayer', {
-            height: '390',
-            width: '640',
-            videoId: urlid //use the id to get the video
-        });
+        // Replace the 'ytplayer' element with an <iframe> and
+        // YouTube player after the API code downloads.
+        var player;
+
+        function onYouTubePlayerAPIReady() {
+            player = new YT.Player('ytplayer', {
+                height: '390',
+                width: '640',
+                videoId: urlid //use the id to get the video
+            });
+        }
+    }
+    else{
+        document.write("No video found!")
     }
 </script>
 
